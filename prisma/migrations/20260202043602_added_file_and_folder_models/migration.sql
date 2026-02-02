8c171facaadbab1817aa96f95e27563ffec5e46d
0x-rekt <kolaysowdarjya@gmail.com>
2026-02-02 10:10:11 +0530

    feat: add Folder and File models with relationships and constraints

---
+++ prisma/migrations/20260202043602_added_file_and_folder_models/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "folder" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "parentId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "file" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "folderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "file_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "folder_userId_idx" ON "folder"("userId");
+
+-- CreateIndex
+CREATE INDEX "file_folderId_idx" ON "file"("folderId");
+
+-- AddForeignKey
+ALTER TABLE "folder" ADD CONSTRAINT "folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "folder" ADD CONSTRAINT "folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "file" ADD CONSTRAINT "file_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20260202043907_modified_models/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `size` to the `file` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `file` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "file" ADD COLUMN     "size" INTEGER NOT NULL,
+ADD COLUMN     "type" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "folder" ALTER COLUMN "parentId" SET DEFAULT '';
